3d98f30bd4abffcf605d2f9ccb4ea4a55092a259
dany536 <deepankar.advisior@gmail.com>
2021-03-04 21:17:15 +0530

    changes in burger component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactusComponent } from './contactus/contactus.component';
+import { MenuComponent } from './menu/menu.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { SidesComponent } from './sides/sides.component';
+import { BurgerComponent } from './burger/burger.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SidesComponent},
+  { path: 'contactus', component: ContactusComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'burger', component: BurgerComponent},
+  { path: 'sides', component: SidesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ContactusComponent } from './contactus/contactus.component';
+import { MenuComponent } from './menu/menu.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { BurgerComponent } from './burger/burger.component';
+import { SidesComponent } from './sides/sides.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactusComponent,
+    MenuComponent,
+    GalleryComponent,
+    BurgerComponent,
+    SidesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
